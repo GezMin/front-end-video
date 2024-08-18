efb4ab4d1652b8e1190f35c250fc369221d574c1
GezMin <s.shvetsov.v@gmail.com>
2024-08-19 00:14:18 +0500

    added premium price

--- src/app/(main)/premium/page.tsx
+++ src/app/(main)/premium/page.tsx
@@ -1,9 +1,11 @@
 import type { Metadata } from 'next'
 
+import Premium from './Premium'
+
 export const metadata: Metadata = {
 	title: 'Премиум подписка'
 }
 
 export default function PremiumPage() {
-	return <div>Premium</div>
+	return <Premium />
 }
